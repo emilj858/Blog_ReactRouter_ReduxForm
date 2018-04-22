787a1b967fe1f128f21741382dd7a95d184b5d4d
emilj858 <teamgoout@gmail.com>
2018-04-22 08:38:32 -0700

    added further form validation and added a helper function to navigate back to the index page once a successful blog post is made

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,9 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { reduxForm } from 'redux-form';
 import { createPost } from '../actions/index';
+import { Link } from 'react-router'
 
 
 class PostsNew extends Component {
+    static contextTypes = {
+        router: PropTypes.object
+    };
+
+    onSubmit(props) {
+        this.props.createPost(props)
+        .then(() => {
+            // blog post has been created navigate user to index
+            // we navigate by using this.content.router.push
+            // with new path to navigate to
+            this.context.router.push('/');
+        });
+    }
+
+
     render() {
         const { fields: {title, categories, content}, handleSubmit } = this.props;
         // same as const handleSubmit = this.props.handleSubmit
@@ -11,29 +27,53 @@ class PostsNew extends Component {
 
 
         return (
-            <form onSubmit={handleSubmit(this.props.createPost)}>
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
                 <h3>Create a new post</h3>
-                <div className="form-group">
+
+                <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
                     <label>Title</label>
                     <input type="text" className="form-control" {...title} />
+                    <div className="text-help">
+                        {title.touched ? title.error : ''}
+                    </div>
                 </div>
 
-                <div className="form-group">
+                <div className={`form-group ${categories.touched && categories.invalid ? 'has-danger' : ''}`}>
                     <label>Categories</label>
                     <input type="text" className="form-control" {...categories}/>
+                    <div className="text-help">
+                        {categories.touched ? categories.error : ''}
+                    </div>
                 </div>
-                <div className="form-group">
+
+                <div className={`form-group ${content.touched && content.invalid ? 'has-danger' : ''}`}>
                     <label>Content</label>
-                    <textarea type="text" className="form-control" />
+                    <textarea type="text" className="form-control" {...content} />
+                    <div className="text-help">
+                        {content.touched ? content.error : ''}
+                    </div>
                 </div>
                 <button type="submit" className="btn btn-primary">Submit</button>
+                <Link to="/" className="btn btn-danger">Cancel</Link>
             </form>
         )
     }
 }
 
 function validate(values) {
-    const erros = {};
+    const errors = {};
+
+    if(!values.title) {
+        errors.title = 'Enter a title'
+    }
+
+    if(!values.categories) {
+        errors.categories = 'Enter a category'
+    }
+
+    if(!values.content) {
+        errors.content = 'Enter some content'
+    }
 
     return errors;
 }
@@ -43,5 +83,5 @@ function validate(values) {
 
 export default reduxForm({
     form: 'PostsNewForm',
-    fields: ['title', 'categories', 'content']
+    fields: ['title', 'categories', 'content'], validate
 }, null, { createPost })(PostsNew);
